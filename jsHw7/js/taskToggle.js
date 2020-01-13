d8c0757e4a7081da584ea9b3820278977934458c
pabadm <balanpasha9@gmail.com>
2020-01-13 23:52:40 +0200

    turnedOffTaskToggle

--- jsHw7/js/taskToggle.js
+++ jsHw7/js/taskToggle.js
@@ -1,14 +1,14 @@
 'use strict';
 
-const tasksScript = document.querySelector('#tasks'); 
-const taskToggleBlock = document.querySelector('#taskToggleBlock');
-const taskToggle = (taskNum) =>{
-    tasksScript.src = `./js/task-${taskNum}.js`;
-    console.log(`switched to task ${taskNum}`);
-}
-for (let index = 1; index <= 10; index++) {
-    taskToggleBlock.insertAdjacentHTML('beforeend', '<button></button>')
-    taskToggleBlock.lastElementChild.addEventListener('click',function(){return taskToggle(index);});
-    taskToggleBlock.lastElementChild.textContent=`task-${index}`;
-    taskToggleBlock.lastElementChild.style.marginLeft = '10px';
-}
+// const tasksScript = document.querySelector('#tasks'); 
+// const taskToggleBlock = document.querySelector('#taskToggleBlock');
+// const taskToggle = (taskNum) =>{
+//     tasksScript.src = `./js/task-${taskNum}.js`;
+//     console.log(`switched to task ${taskNum}`);
+// }
+// for (let index = 1; index <= 10; index++) {
+//     taskToggleBlock.insertAdjacentHTML('beforeend', '<button></button>')
+//     taskToggleBlock.lastElementChild.addEventListener('click',function(){return taskToggle(index);});
+//     taskToggleBlock.lastElementChild.textContent=`task-${index}`;
+//     taskToggleBlock.lastElementChild.style.marginLeft = '10px';
+// }
